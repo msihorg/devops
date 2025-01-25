fde3e08c36220d45f9eb49cb5ea3b736313a9214
Make Sure It Happens Inc <84290024+msihorg@users.noreply.github.com>
2025-01-25 12:01:43 -0500

    Create setup_webserver.sh

---
+++ setup_webserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# make executable: chmod +x script.sh
+
+# Installation PowerShell
+snap install powershell --classic
+
+#Install the cot net SDK 9
+sudo add-apt-repository ppa:dotnet/backports
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-9.0
+
+#Install Nginx:
+sudo apt install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# update firewall
+sudo ufw allow from x.x.x.x/y to any port 80
+sudo ufw allow from x.x.x.x/y to any port 443
