f302735238ecfa71c3542fcec4e6e479b2dc0148
Make Sure It Happens Inc <84290024+msihorg@users.noreply.github.com>
2025-01-25 11:55:38 -0500

    Create Upgrade ubuntu 20.04  to 24.04.sh

---
+++ Upgrade ubuntu 20.04  to 24.04.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# upgrade ubuntu 20.04 to 22.04
+sudo apt update
+sudo apt upgrade
+sudo apt install update-manager-core
+sudo do-release-upgrade
+cat /etc/os-release
+
+# upgrade ubuntu 22.04 to 24.04
+sudo apt update
+sudo apt upgrade
+sudo apt install ubuntu-release-upgrader-core
+cat /etc/os-release
